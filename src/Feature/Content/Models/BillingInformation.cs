cc9514b9079d02ed377224c5dff99992b6889034
Andres Ayala <andres.ayala@outlook.com>
2022-03-05 17:18:39 -0500

    Added Billing form

---
+++ src/Feature/Content/Models/BillingInformation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dresscode.Feature.Content.Models
+{
+    public class BillingInformation
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+    }
+}
